d8edbf9fdb5aaf4760b490b787d6cc2cffee973e
Patrick Mendes Bisolato <patrick.bisolato@uel.br>
2026-04-13 19:50:39 -0300

    Ajuste do Repository da Tarefa

--- Gerenciador_Tarefas/src/main/java/com/tarefas/repository/TarefaRepository.java
+++ Gerenciador_Tarefas/src/main/java/com/tarefas/repository/TarefaRepository.java
@@ -1,18 +1,13 @@
 package com.tarefas.repository;
 
 import com.tarefas.model.Tarefa;
-import com.tarefas.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 
-    List<Tarefa> findByUsuario(Usuario usuario);
+    List<Tarefa> findByUsuarioId(Long usuarioId);
 
-    List<Tarefa> findByUsuarioAndConcluida(Usuario usuario, boolean concluida);
-
-    List<Tarefa> findByUsuarioOrderByCriadoEmDesc(Usuario usuario);
+    List<Tarefa> findByUsuarioIdAndConcluida(Long usuarioId, boolean concluida);
 }
